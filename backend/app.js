552a9b02e179f18cb76777390293cc6dfbf7c492
AlexisDev <82178752+Alexis-Dernoncourt@users.noreply.github.com>
2021-11-05 02:21:00 +0100

    Init. MongoDB + mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://dbAdmin:AeSVbvs98yR4pycs@cluster0.fmlqg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res) => {
     res.json({ message: 'Votre requête a bien été reçue !' }); 
