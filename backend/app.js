c2800c3715d6c4ea8e3adbaea65c7ba49cdd6153
AlexisDev <82178752+Alexis-Dernoncourt@users.noreply.github.com>
2021-11-05 04:34:03 +0100

    Step2 : build user journey

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const config = require('./config');
+const userRoutes = require('./routes/users');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://dbAdmin:AeSVbvs98yR4pycs@cluster0.fmlqg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+// DB CONNEXION
+mongoose.connect(`mongodb+srv://${config.db.user}:${config.db.password}@cluster0.fmlqg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
+
+// app.use((req, res) => {
+//     res.json({ message: 'Votre requête a bien été reçue !' }); 
+// });
+
+//app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+
 
 module.exports = app;
---
+++ backend/config.js
@@ -0,0 +1,6 @@
+const db = {
+    user: 'dbAdmin',
+    password: 'AeSVbvs98yR4pycs'
+};
+
+exports.db = db;
